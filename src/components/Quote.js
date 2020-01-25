3cf7f5e63c8be4969f5dae048d34161def89a7af
Lauraklijn <lauraklijn12@hotmail.com>
2020-01-25 19:08:44 +0100

    final adds

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import QuoteSearcher from "./QuoteSearcher";
+import "./ColorQuotes.css";
 
 export default class Quote extends Component {
   state = {
-    likes: 0,
-    disLikes: 0
+    likes: false,
+    disLikes: false
   };
 
   // Likes (Step 4 --> Get like):
-  incrementLikes = () => {
+  addLikes = () => {
     console.log("Likes?", this.state.likes);
     this.setState({
       likes: this.state.likes + 1
@@ -24,18 +24,18 @@ export default class Quote extends Component {
     return this.props.getLikes(this.props.id);
   };
 
-  // Dislikes (step 4 --> Lifting state):
-  decrementLikes = () => {
+  // Dislikes (step 4 --> Get disLike):
+  adddisLikes = () => {
     console.log("dislikes?", this.state.disLikes);
     this.setState({
       disLikes: this.state.disLikes + 1
     });
   };
 
-  // Dislikes (step 5 --> (Lifting state)
+  //Dislikes (step 5 --> (updating dislikes)
   handleClickDisLike = () => {
     this.setState({
-      likes: false,
+      like: false,
       disLikes: true
     });
     return this.props.getDisLikes(this.props.id);
@@ -44,18 +44,21 @@ export default class Quote extends Component {
   render() {
     return (
       <div>
-        <p>{this.props.text}</p>
-        <p>
-          {"By:"} {this.props.author}
-        </p>
-        <p>
-          <div>
-            <button onClick={this.handleClickLike}>Like</button>
-          </div>
-          <div>
-            <button onClick={this.handleClickDisLike}>Dislike</button>
-          </div>
-        </p>
+        {this.state.likes ? (
+          <p className="getGreen"> {this.props.text}</p>
+        ) : this.state.disLikes ? (
+          <p className="getRed">{this.props.text}</p>
+        ) : (
+          <p> {this.props.text}</p>
+        )}
+
+        <p className="author">By: {this.props.author} </p>
+        <div>
+          <button onClick={this.handleClickLike}>Like</button>
+        </div>
+        <div>
+          <button onClick={this.handleClickDisLike}>Dislike</button>
+        </div>
       </div>
     );
   }
--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import Quote from "./Quote";
-import { queryByTitle } from "@testing-library/react";
 
 export default class QuoteSearcher extends Component {
   state = {
     quotes: [],
     loading: true,
+    error: false,
     likesCounter: 0,
     disLikeCounter: 0
   };
@@ -14,22 +14,23 @@ export default class QuoteSearcher extends Component {
     fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
       .then(response => response.json())
       .then(json => {
-        this.setState({ quotes: json.results, loading: false });
-      });
+        this.setState({
+          quotes: json.results,
+          loading: false
+        });
+      })
+      .catch(console.error);
   }
 
   // Sum likes(lifting state) by id to get it in the likeCounter
   getLikes = id => {
     const addLikes = this.state.quotes.map(quote => {
       if (quote.id === id) {
-        return { ...quote, likes: quote.likes + 1 };
       }
       return quote;
     });
     this.setState({
       quotes: addLikes,
-      likes: true,
-      disLikes: false,
       likesCounter: this.state.likesCounter + 1
     });
   };
@@ -44,8 +45,6 @@ export default class QuoteSearcher extends Component {
     });
     this.setState({
       quotes: addDisLikes,
-      likes: false,
-      disLikes: true,
       disLikeCounter: this.state.disLikeCounter + 1
     });
   };
@@ -54,32 +53,41 @@ export default class QuoteSearcher extends Component {
     console.log("See quotes?", this.state.quotes);
     if (this.state.loading) {
       return <div> One moment please, loading...</div>;
-    }
-
-    return (
-      <div>
-        <h1>Quotes</h1>
-        <h2>
-          liked: {this.state.likesCounter} / disliked:{" "}
-          {this.state.disLikeCounter}
-        </h2>
-        <button>Search for Quotes!</button>
-        {!this.state.fetching &&
-          this.state.quotes
-            .sort((quote1, quote2) => quote2.likes - quote1.likes)
-            .map(quote => (
-              <Quote
-                key={Math.random()}
-                id={quote.id}
-                text={quote.quoteText}
-                author={quote.quoteAuthor}
-                likes={quote.likes}
-                disLikes={quote.disLikes}
-                getLikes={this.getLikes}
-                getDisLikes={this.getDisLikes}
-              />
-            ))}
-      </div>
-    );
+    } else if (this.state.error) {
+      return <div> Oops, something went wrong!</div>;
+    } else
+      return (
+        <div>
+          <h1>Quotes</h1>
+          <h2>
+            liked: {this.state.likesCounter} / disliked:{" "}
+            {this.state.disLikeCounter}
+          </h2>
+          <h3>
+            <form onSubmit={this.onSubmit}>
+              <label>
+                Search for a quote:{" "}
+                <input type="text" placeholder="Search for a quote" />
+              </label>
+              <input type="submit" value="Search" />
+            </form>
+          </h3>
+          {!this.state.fetching &&
+            this.state.quotes
+              .sort((quote1, quote2) => quote2.likes - quote1.likes)
+              .map(quote => (
+                <Quote
+                  //key={Math.random()} --> als ik dit doe, dan is m'n waarschuwing weg in Dev console, maar dan doen m'n kleuren het niet meer
+                  text={quote.quoteText}
+                  author={quote.quoteAuthor}
+                  likes={quote.likes}
+                  disLikes={quote.disLikes}
+                  getLikes={this.getLikes}
+                  getDisLikes={this.getDisLikes}
+                  id={quote.id}
+                />
+              ))}
+        </div>
+      );
   }
 }
