edb8736e57436e76bdaad9208b14ee391f681691
Lauraklijn <lauraklijn12@hotmail.com>
2020-01-25 14:30:21 +0100

    Lifted state (Sum Likes --> part 5

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,61 @@
 import React, { Component } from "react";
+import QuoteSearcher from "./QuoteSearcher";
 
 export default class Quote extends Component {
+  state = {
+    likes: 0,
+    disLikes: 0
+  };
+
+  // Likes (Step 4 --> Get like):
+  incrementLikes = () => {
+    console.log("Likes?", this.state.likes);
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+
+  // Likes total (Step 5 --> (updating likes))
+  handleClickLike = () => {
+    this.setState({
+      likes: true,
+      disLikes: false
+    });
+    return this.props.getLikes(this.props.id);
+  };
+
+  // Dislikes (step 4 --> Lifting state):
+  decrementLikes = () => {
+    console.log("dislikes?", this.state.disLikes);
+    this.setState({
+      disLikes: this.state.disLikes + 1
+    });
+  };
+
+  // Dislikes (step 5 --> (Lifting state)
+  handleClickDisLike = () => {
+    this.setState({
+      likes: false,
+      disLikes: true
+    });
+    return this.props.getDisLikes(this.props.id);
+  };
+
   render() {
     return (
       <div>
         <p>{this.props.text}</p>
-        <p>{this.props.author}</p>
+        <p>
+          {"By:"} {this.props.author}
+        </p>
+        <p>
+          <div>
+            <button onClick={this.handleClickLike}>Like</button>
+          </div>
+          <div>
+            <button onClick={this.handleClickDisLike}>Dislike</button>
+          </div>
+        </p>
       </div>
     );
   }
--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Quote from "./Quote";
+import { queryByTitle } from "@testing-library/react";
 
 export default class QuoteSearcher extends Component {
   state = {
     quotes: [],
-    loading: true
+    loading: true,
+    likesCounter: 0,
+    disLikeCounter: 0
   };
 
   componentDidMount() {
@@ -15,6 +18,38 @@ export default class QuoteSearcher extends Component {
       });
   }
 
+  // Sum likes(lifting state) by id to get it in the likeCounter
+  getLikes = id => {
+    const addLikes = this.state.quotes.map(quote => {
+      if (quote.id === id) {
+        return { ...quote, likes: quote.likes + 1 };
+      }
+      return quote;
+    });
+    this.setState({
+      quotes: addLikes,
+      likes: true,
+      disLikes: false,
+      likesCounter: this.state.likesCounter + 1
+    });
+  };
+
+  // Sum dislikes(lifting state) by id to get it in the disLikeCounter
+  getDisLikes = id => {
+    const addDisLikes = this.state.quotes.map(quote => {
+      if (quote.id === id) {
+        return { ...quote, disLikes: quote.disLikes + 1 };
+      }
+      return quote;
+    });
+    this.setState({
+      quotes: addDisLikes,
+      likes: false,
+      disLikes: true,
+      disLikeCounter: this.state.disLikeCounter + 1
+    });
+  };
+
   render() {
     console.log("See quotes?", this.state.quotes);
     if (this.state.loading) {
@@ -24,13 +59,26 @@ export default class QuoteSearcher extends Component {
     return (
       <div>
         <h1>Quotes</h1>
-        {this.state.quotes.map(quote => (
-          <Quote
-            text={quote.quoteText}
-            author={quote.quoteAuthor}
-            key={quote.id}
-          />
-        ))}
+        <h2>
+          liked: {this.state.likesCounter} / disliked:{" "}
+          {this.state.disLikeCounter}
+        </h2>
+        <button>Search for Quotes!</button>
+        {!this.state.fetching &&
+          this.state.quotes
+            .sort((quote1, quote2) => quote2.likes - quote1.likes)
+            .map(quote => (
+              <Quote
+                key={Math.random()}
+                id={quote.id}
+                text={quote.quoteText}
+                author={quote.quoteAuthor}
+                likes={quote.likes}
+                disLikes={quote.disLikes}
+                getLikes={this.getLikes}
+                getDisLikes={this.getDisLikes}
+              />
+            ))}
       </div>
     );
   }
